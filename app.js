b73f6ea56f3beea85b8e15e2cb59c666a415b1e2
namiqmamedov <namigmm@code.edu.az>
2022-09-09 04:38:26 +0400

    JS

---
+++ app.js
@@ -0,0 +1,20 @@
+// if (confirm("Choose one.") == true) {
+//     document.body.style.backgroundColor = "black";    
+// }
+
+// var age = prompt("How old are you?");
+
+// if (age >= 18)
+// {
+//     document.body.style.background = 'Yellow';
+// }
+// else
+// {
+//     alert("403: Access Denied");
+// }
+ 
+
+
+
+
+
