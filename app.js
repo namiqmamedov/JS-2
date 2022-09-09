3d32f73520813ab05d7400501ae2ac73b50afcc1
Namiq <107476714+namiqmamedov@users.noreply.github.com>
2022-09-09 17:11:27 +0400

    JS

--- app.js
+++ app.js
@@ -15,6 +15,23 @@
  
 
 
+function chColor(){
+    var input = document.getElementById("Group").value;
+    if(input.toUpperCase().startsWith('P') && input.length === 4){
+        if(input.includes("1",1)){
+            document.body.style.backgroundColor = "red"
+        }else if(input.includes("2",1)){
+            document.body.style.backgroundColor = "yellow"
+        }else if(input.includes("3",1)){
+            document.body.style.backgroundColor = "black"
+        }else{
+            console.log("Tapilmadi");
+        }
+    }else{
+        alert("Wrong")
+    }
+}
+ 
 
 
 
